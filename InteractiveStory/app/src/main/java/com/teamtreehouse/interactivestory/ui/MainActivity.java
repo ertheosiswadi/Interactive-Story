d4f1362b5330e5df87a38d12ea1c62c8d077a40c
ertheosiswadi <38580138+ertheosiswadi@users.noreply.github.com>
2018-10-17 05:52:57 -0700

    1stcommit
    1stcommit

---
+++ InteractiveStory/app/src/main/java/com/teamtreehouse/interactivestory/ui/MainActivity.java
@@ -0,0 +1,51 @@
+package com.teamtreehouse.interactivestory.ui;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.teamtreehouse.interactivestory.R;
+import com.teamtreehouse.interactivestory.model.Page;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText)findViewById(R.id.nameEditText);
+        startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                String name = nameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+        nameField.setText("");
+    }
+
+    private void startStory(String name)
+    {
+        Intent intent = new Intent(this, StoryActivity.class);
+        Resources resources =  getResources();
+        String key = getString(R.string.key_name);
+        intent.putExtra("name", name);
+        startActivity(intent);
+    }
+}
